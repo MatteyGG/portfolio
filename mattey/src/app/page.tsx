61d0bdac6c599288e22a1624ad045edcaf76ad43
MatteyGG <matgaranin@gmail.com>
2024-08-27 15:31:16 +0300

    Base and home page root

---
+++ mattey/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-row p-64 space-x-24 justify-center">
+      <div className="text-9xl font-bold text-gray-100 type-code"></div>
+      <div className="text-9xl font-bold">design </div>
+    </main>
+  );
+}
