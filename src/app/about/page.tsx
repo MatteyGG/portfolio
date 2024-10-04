eb3b34bc4f1b4c3f9b38c4fde60b6f909d94e71c
MatteyGG <matgaranin@gmail.com>
2024-10-04 23:23:31 +0300

    Added skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,21 +11,26 @@ export default function About() {
             <Image
               src="https://avatars.githubusercontent.com/u/49521885?v=4"
               alt="Matvey's avatar"
-              width={128}
-              height={128}
+              width={256}
+              height={256}
               className="rounded-full"
             />
           </div>
-          <h1 className="text-3xl font-bold">Matvey</h1>
-          <p className="text-center text-xl">
-            Hi, I&#39;m a self-taught frontend developer from Russia. I&#39;m currently
+
+          <h1 className="text-4xl font-bold">
+            <> Hi there, I&#39;m Mattey </>
+          </h1>
+          <p className="text-center text-2xl">
+            A self-taught frontend developer from Russia. I&#39;m currently
             working on my own projects.
           </p>
-          <p className="text-center text-xl">
-            I&#39;m passionate about web development, game development and automate all the things.
+          <p className="text-center text-2xl">
+            I&#39;m passionate about web development, game development and
+            automate all the things.
           </p>
-          <div className="flex gap-4 items-center ">
-            <a className="animate-bounce delay-150 hover:animate-none ease-in-out"
+          <div className="flex gap-6 items-center mt-4 ">
+            <a
+              className="animate-bounce delay-150 hover:animate-none ease-in-out"
               href="https://t.me/ser_dantes_r"
               target="_blank"
               rel="noopener noreferrer"
@@ -33,34 +38,93 @@ export default function About() {
               <Image
                 src="/telegram.svg"
                 alt="Telegram"
-                width={35}
-                height={35}
+                width={50}
+                height={50}
               />
             </a>
-            <a className="animate-bounce delay-500 hover:animate-none ease-in-out"
+            <a
+              className="animate-bounce delay-500 hover:animate-none ease-in-out"
               href="https://github.com/MatteyGG"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src="/github.svg" alt="GitHub" width={35} height={35} />
+              <Image src="/github.svg" alt="Github" width={50} height={50} />
             </a>
-            <a className="animate-bounce delay-1000 hover:animate-none ease-in-out"
+            <a
+              className="animate-bounce delay-1000 hover:animate-none ease-in-out"
               href="mailto: matgaranin@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                src="/gmail.svg"
-                alt="Gmail"
-                width={35}
-                height={35}
-              />
+              <Image src="/gmail.svg" alt="Gmail" width={50} height={50} />
             </a>
+          </div>
+          {/* Skills */}
+          <div className="grid grid-cols-3 gap-6 place-items-center text-center w-full">
+            <div className="col-start-0 col-end-3 ">
+              <div className="items-center w-full">
+                <h2 className="text-3xl font-bold">
+                  Languages and tools
+                </h2>
+                <div className="flex gap-6 mt-6">
+                  <Image src="/html.svg" alt="HTML" width={60} height={60} />
+                  <Image src="/css.svg" alt="CSS" width={60} height={60} />
+                  <Image
+                    src="/javascript.svg"
+                    alt="JavaScript"
+                    width={60}
+                    height={60}
+                  />
+                  <Image
+                    src="/python.svg"
+                    alt="TypeScript"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-start-1 -mt-4">
+              <div className="items-center">
+                <h2 className="text-2xl font-bold">JavaScript</h2>
+                <div className="flex gap-2">
+                  <Image src="/react.svg" alt="HTML" width={60} height={60} />
+                  <Image src="/next.svg" alt="HTML" width={60} height={60} />
+                  <Image src="/prism-sharp.svg" alt="HTML" width={60} height={60} />
+                </div>
+              </div>
+            </div>
+            <div className="col-start-2 -mt-4">
+              <div className="items-center">
+                <h2 className="text-2xl font-bold">Python</h2>
+                <div className="flex gap-2">
+                  <Image src="/aiogram.webp" alt="HTML" width={60} height={60} />
+                  <Image src="/next.svg" alt="HTML" width={60} height={60} />
+                </div>
+              </div>
+            </div>
+            <div className="col-start-3 -mt-4">
+              <div className="mt-4 items-center">
+                <h2 className="text-2xl font-bold">Other</h2>
+                <div className="flex gap-2 mt-6 items-center">
+                  <Image
+                    src="/github_tech.svg"
+                    alt="HTML"
+                    width={60}
+                    height={60}
+                  />
+                  <Image
+                    src="/postgres.svg"
+                    alt="HTML"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 }
-
-
--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -8,7 +8,11 @@ function NavBar() {
       <nav className="hidden md:block backdrop-blur-lg py-4">
         <div className="flex justify-between items-center mx-8">
           <div className="flex text-3xl  items-center">
-            <Link href="/">MatteyGG</Link>
+            <div className="flex items-center gap-8 animate-pulse duration-700">
+              <button className="flex items-center gap-0.5 font-bold text-xl">
+                <Link href="/about">About me</Link>
+              </button>
+            </div>
           </div>
           <ul className="flex justify-center items-center gap-8">
             <li className="upper_animation">
@@ -21,25 +25,25 @@ function NavBar() {
               <Link href="/design">Design</Link>
             </li>
           </ul>
-          <div className="flex gap-8">
-            <div className="flex items-center gap-8 animate-pulse duration-700">
-              <button className="flex items-center gap-0.5 font-bold text-xl">
-                <Link href="/about">About me</Link>
-              </button>
-            </div>
-
-            <button
-              className="text-xl"
-              onClick={() => {
-                document.documentElement.classList.toggle("dark");
-                localStorage.setItem(
-                  "theme",
-                  document.documentElement.classList.toString()
-                );
-              }}
-            >
-              Dark mode
-            </button>
+          <div className="flex gap-4">
+            <label className="inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                onClick={() => {
+                  {
+                    document.documentElement.classList.toggle("dark");
+                    localStorage.setItem(
+                      "theme",
+                      document.documentElement.classList.toString()
+                    );
+                  }
+                }}
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gary-300 dark:peer-focus:ring-gray-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+            </label>
           </div>
         </div>
       </nav>
