4741984ef22df82e734f5f8990f1d8542d9da6c8
MatteyGG <matgaranin@gmail.com>
2024-10-04 23:26:48 +0300

    quick fix

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 interface GalleryProps {
-  imageUrl: string;
+  imageUrl: string[];
   topic: string;
   describtion: string;
   redirect: string;
