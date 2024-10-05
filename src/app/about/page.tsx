bad39b8b0f6a60adfd70255b2fc393a420cc6dd3
MatteyGG <matgaranin@gmail.com>
2024-10-05 15:35:02 +0300

    added some more skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <>
       <NavBar />
-      <div className="flex h-screen justify-center items-center">
+      <div className="flex mt-12 justify-center items-center overscroll-none">
         <div className="flex flex-col items-center gap-4">
           <div className="avatar">
             <Image
@@ -15,20 +15,7 @@ export default function About() {
               height={256}
               className="rounded-full"
             />
-          </div>
-
-          <h1 className="text-4xl font-bold">
-            <> Hi there, I&#39;m Mattey </>
-          </h1>
-          <p className="text-center text-2xl">
-            A self-taught frontend developer from Russia. I&#39;m currently
-            working on my own projects.
-          </p>
-          <p className="text-center text-2xl">
-            I&#39;m passionate about web development, game development and
-            automate all the things.
-          </p>
-          <div className="flex gap-6 items-center mt-4 ">
+            <div className="flex gap-12 items-center ">
             <a
               className="animate-bounce delay-150 hover:animate-none ease-in-out"
               href="https://t.me/ser_dantes_r"
@@ -59,14 +46,28 @@ export default function About() {
               <Image src="/gmail.svg" alt="Gmail" width={50} height={50} />
             </a>
           </div>
+          </div>
+          <div className="text-center text-xl md:text-2xl">
+          <h1 className="text-4xl font-bold">
+            <> Hi there, I&#39;m Mattey </>
+          </h1>
+          <p className="">
+            A self-taught frontend developer from Russia. I&#39;m currently
+            working on my own projects.
+          </p>
+          <p className="">
+            I&#39;m passionate about web development, game development and
+            automate all the things.
+          </p>
+          </div>
           {/* Skills */}
-          <div className="grid grid-cols-3 gap-6 place-items-center text-center w-full">
-            <div className="col-start-0 col-end-3 ">
-              <div className="items-center w-full">
+          <div className="grid md:grid-cols-3  gap-6 mt-4 place-items-center text-center w-full">
+            <div className="col-span-full">
+              <div className="">
                 <h2 className="text-3xl font-bold">
                   Languages and tools
                 </h2>
-                <div className="flex gap-6 mt-6">
+                <div className="flex w-full gap-6 mt-6">
                   <Image src="/html.svg" alt="HTML" width={60} height={60} />
                   <Image src="/css.svg" alt="CSS" width={60} height={60} />
                   <Image
@@ -77,45 +78,51 @@ export default function About() {
                   />
                   <Image
                     src="/python.svg"
-                    alt="TypeScript"
+                    alt="Python"
                     width={60}
                     height={60}
                   />
                 </div>
               </div>
             </div>
-            <div className="col-start-1 -mt-4">
+            <div className=" -mt-4">
               <div className="items-center">
                 <h2 className="text-2xl font-bold">JavaScript</h2>
                 <div className="flex gap-2">
-                  <Image src="/react.svg" alt="HTML" width={60} height={60} />
-                  <Image src="/next.svg" alt="HTML" width={60} height={60} />
-                  <Image src="/prism-sharp.svg" alt="HTML" width={60} height={60} />
+                  <Image src="/react.svg" alt="React" width={60} height={60} />
+                  <Image src="/next.svg" alt="NextJS" width={60} height={60} />
+                  <Image src="/prism-sharp.svg" alt="Prism" width={60} height={60} />
                 </div>
               </div>
             </div>
-            <div className="col-start-2 -mt-4">
+            <div className="-mt-4">
               <div className="items-center">
                 <h2 className="text-2xl font-bold">Python</h2>
                 <div className="flex gap-2">
-                  <Image src="/aiogram.webp" alt="HTML" width={60} height={60} />
-                  <Image src="/next.svg" alt="HTML" width={60} height={60} />
+                  <Image src="/aiogram.webp" alt="Aiogram" width={60} height={60} />
+                  <Image src="/taipy.png" alt="Taipy" width={60} height={60} />
                 </div>
               </div>
             </div>
-            <div className="col-start-3 -mt-4">
+            <div className="-mt-4">
               <div className="mt-4 items-center">
                 <h2 className="text-2xl font-bold">Other</h2>
-                <div className="flex gap-2 mt-6 items-center">
+                <div className="flex gap-2 items-center">
                   <Image
-                    src="/github_tech.svg"
-                    alt="HTML"
+                    src="/git_tech.svg"
+                    alt="git"
+                    width={60}
+                    height={60}
+                  />
+                  <Image
+                    src="/docker.svg"
+                    alt="Docker"
                     width={60}
                     height={60}
                   />
                   <Image
                     src="/postgres.svg"
-                    alt="HTML"
+                    alt="PostgreSQL"
                     width={60}
                     height={60}
                   />
