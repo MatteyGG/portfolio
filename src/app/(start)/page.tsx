b786c54937086f6d9abf466d35e7c148dd634051
MatteyGG <matgaranin@gmail.com>
2025-02-01 20:27:48 +0300

    modified:   src/app/(start)/page.tsx

--- src/app/(start)/page.tsx
+++ src/app/(start)/page.tsx
@@ -4,9 +4,22 @@ export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex-row mx-auto">
-        <div className="font-bold text-6xl upper_animation"><button><Link href="/code">Code<span className="blink">|</span></Link></button></div>
-        <p className="text-4xl translate-x-2 text-slate-700">or</p>
-        <div className="font-bold text-6xl upper_animation"><button><Link href="/design">design <span className="rainbow">∆</span></Link></button> </div>
+        <p className="text-gray-500 text-lg ml-1">Choose:</p>
+        <div className="font-bold text-6xl upper_animation">
+          <button>
+            <Link href="/code">
+              Code<span className="blink">|</span>
+            </Link>
+          </button>
+        </div>
+        <p className="text-4xl translate-x-2 text-gray-500">OR</p>
+        <div className="font-bold text-6xl upper_animation">
+          <button>
+            <Link href="/design">
+              design <span className="rainbow">∆</span>
+            </Link>
+          </button>{" "}
+        </div>
       </div>
     </div>
   );
